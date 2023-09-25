b32cdcc98dd6dde604e07d2bdca0e35f341e0fdf
leabgt <lbugeat@gmail.com>
2023-09-25 13:31:42 +0200

    Update CSS Form Event

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,29 +21,7 @@ class EventType extends AbstractType
         $builder
             ->add('title', TextType::class, [
                 'row_attr' => ['class' => 'event-form-row'],
-            ])
-            ->add('place', TextType::class, [
-                'row_attr' => ['class' => 'event-form-row'],
-            ])
-            ->add('startDate', DateTimeType::class, [
-                'row_attr' => ['class' => 'event-form-row'],
-            ])
-            ->add('endDate', DateTimeType::class, [
-                'row_attr' => ['class' => 'event-form-row'],
-            ])
-            ->add('isPrivate', CheckboxType::class, [
-                'row_attr' => ['class' => 'event-form-row'],
-            ])
-            ->add('isFinancialParticipation', CheckboxType::class, [
-                'required' => false,
-                'row_attr' => ['class' => 'event-form-row'],
-                'attr' => ['id' => 'event_isFinancialParticipation'], // Ajoutez l'ID
-            ])
-            ->add('financialParticipationAmount', TextType::class, [
-                'label' => false,
-                'required' => false,
-                'row_attr' => ['class' => 'event-form-row'],
-                'attr' => ['id' => 'event_financialParticipationAmount'], // Ajoutez l'ID
+                'label' => 'Nom de l\'événement',
             ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
@@ -50,6 +29,40 @@ class EventType extends AbstractType
                 'choice_label' => 'name',
                 'placeholder' => 'Select a category', // Texte par défaut pour le champ (optionnel)
                 'multiple' => false, // Liste déroulante (par défaut)
+                'label' => 'Catégorie',
+            ])
+            ->add('place', TextType::class, [
+                'row_attr' => ['class' => 'event-form-row'],
+                'label' => 'Lieu / Adresse',
+            ])
+            ->add('startDate', DateTimeType::class, [
+                'row_attr' => ['class' => 'event-form-row'],
+                'label' => 'Début',
+            ])
+            ->add('endDate', DateTimeType::class, [
+                'row_attr' => ['class' => 'event-form-row'],
+                'label' => 'Fin',
+            ])
+            ->add('isPrivate', CheckboxType::class, [
+                'row_attr' => ['class' => 'event-form-row'],
+                'label' => 'Cet événement est-il privé ?',
+            ])
+            ->add('isFinancialParticipation', CheckboxType::class, [
+                'required' => false,
+                'row_attr' => ['class' => 'event-form-row'],
+                'attr' => ['id' => 'event_isFinancialParticipation'], // Ajoutez l'ID
+                'label' => 'Y-a-t-il une participation financière ?',
+            ])
+            ->add('financialParticipationAmount', TextType::class, [
+                'label' => false,
+                'required' => false,
+                'row_attr' => ['class' => 'event-form-row'],
+                'attr' => ['id' => 'event_financialParticipationAmount'], // Ajoutez l'ID
+                'label' => 'Montant de la participation',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer un nouvel événement',
+                'attr' => ['class' => 'btn btn-form'],
             ]);
     }
 
